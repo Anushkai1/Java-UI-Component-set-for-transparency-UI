b0e8056725fcd00543a62301a0e8d5aeb3fa25b4
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-04-26 16:45:22 +0530

    Update mbg.java

--- src/Assest/mbg.java
+++ src/Assest/mbg.java
@@ -24,5 +24,8 @@ public class mbg extends javax.swing.JPanel {
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
+
+    }
 }
