9ffff700149ed1f79bf94050f915d7f20a073122
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-02 16:58:36 +0530

    Update skin.java

--- src/Assest/skin.java
+++ src/Assest/skin.java
@@ -8,7 +8,12 @@ import java.io.InputStream;
 
 public class skin {
     
+    // font return
+    public static Font ttf( float fsize, int sel){
+        String path = "SairaSemiCondensed-Medium.ttf";
 
+        
+    }
     
 
 
