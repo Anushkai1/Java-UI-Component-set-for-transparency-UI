e96bd21d53192db71f8f2e66c40a736b25ef56c2
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-05 17:01:42 +0530

    Update skin.java

--- src/Assest/skin.java
+++ src/Assest/skin.java
@@ -46,6 +46,9 @@ public class skin {
     public static Color blc2 = new Color(13,13,13);
     public static Color blc3 = new Color(25,25,25);
     public static Color blc4 = new Color(40,40,40);
-
+    public static Color blc5 = new Color(64,64,64);
+    public static Color blc6 = new Color(13,13,13, 30);
+  
+    
 
 }
