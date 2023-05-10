4641f06e63ead0b1843b7e94d851dc19196ac88c
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-10 07:26:18 +0530

    Update ScrollBarCustome.java

--- src/Elements/ScrollBarCustome.java
+++ src/Elements/ScrollBarCustome.java
@@ -9,7 +9,13 @@ import Assest.skin;
 public class ScrollBarCustome extends JScrollBar{
     
     
-
+    public ScrollBarCustome(){
+        setUI(new ModernScrollBarUI());
+        setPreferredSize(new Dimension(5, 5));
+        setForeground(skin.bc4);
+        setBackground(skin.wc9);
+        
+    }
     
 
 
