eb2c3d5da0c2849bfc5a206127d60bbf1a6d3bd5
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-11 07:29:11 +0530

    Update WCloseBtn.java

--- src/Elements/WCloseBtn.java
+++ src/Elements/WCloseBtn.java
@@ -13,7 +13,13 @@ public class WCloseBtn extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        iBtn2 = new Swing.iBtn();
 
+        setBackground(new java.awt.Color(0, 0, 0));
+        setPreferredSize(new java.awt.Dimension(53, 30));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+  
         
     }// </editor-fold>//GEN-END:initComponents
 
