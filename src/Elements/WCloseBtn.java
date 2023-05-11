eff8911504e38d5c2d520f57efc909d1289569de
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-11 07:29:18 +0530

    Update WCloseBtn.java

--- src/Elements/WCloseBtn.java
+++ src/Elements/WCloseBtn.java
@@ -19,7 +19,14 @@ public class WCloseBtn extends javax.swing.JPanel {
         setPreferredSize(new java.awt.Dimension(53, 30));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-  
+        iBtn2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/close.png"))); // NOI18N
+        iBtn2.setMargin(new java.awt.Insets(5, 5, 5, 5));
+        iBtn2.setPreferredSize(new java.awt.Dimension(53, 30));
+        iBtn2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                iBtn2MouseClicked(evt);
+            }
+        });
         add(iBtn2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
