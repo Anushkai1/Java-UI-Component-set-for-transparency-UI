02b1c95bad6ff769dbbe348c90cf58e4b287fed8
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-11 07:29:08 +0530

    Update WCloseBtn.java

--- src/Elements/WCloseBtn.java
+++ src/Elements/WCloseBtn.java
@@ -9,6 +9,13 @@ public class WCloseBtn extends javax.swing.JPanel {
     }
 
 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+
+        
+    }// </editor-fold>//GEN-END:initComponents
 
 
 }
