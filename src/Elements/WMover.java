cb03b28616abca38466e927278f1e7d849eb8b62
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-15 07:37:58 +0530

    Update WMover.java

---
+++ src/Elements/WMover.java
@@ -0,0 +1,22 @@
+
+package Elements;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JFrame;
+
+public class WMover extends javax.swing.JPanel {
+
+    public WMover() {
+        initComponents();
+        setOpaque(false);
+        
+        setRequestFocusEnabled(false);
+    }
+
+
+
+
+}
