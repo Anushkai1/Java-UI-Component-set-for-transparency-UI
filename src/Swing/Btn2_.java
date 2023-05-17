91b1e217022099958d870a839166e07649be2c67
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-17 07:45:44 +0530

    Update Btn2_.java

--- src/Swing/Btn2_.java
+++ src/Swing/Btn2_.java
@@ -73,7 +73,29 @@ public class Btn2_ extends JButton{
         setContentAreaFilled(false);
                 setBorderPainted(false);
         setRequestFocusEnabled(false);
-   
+        //mouse evet
+        addMouseListener(new MouseAdapter(){
+            @Override
+            public void mousePressed(MouseEvent e){
+                if(isEnabled()){
+                    setBackground(active_bg);
+                    setForeground(active_fg);
+                }
+            }
+            
+            @Override
+            public  void mouseReleased(MouseEvent e){
+                setBackground(bg);
+                setForeground(fg);
+                if(over && isEnabled()){
+                    setBackground(hover_bg);
+                    setForeground(hover_fg);
+                }
+            }
+            
+
+        
+        });    
     }
 
     private boolean over;
