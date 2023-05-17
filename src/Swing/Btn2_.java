45734c5cade9f6dfdd016b80f9ca8a8f6f9d811f
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-17 07:45:35 +0530

    Update Btn2_.java

--- src/Swing/Btn2_.java
+++ src/Swing/Btn2_.java
@@ -10,3 +10,20 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import Assest.skin;
 import java.awt.Dimension;
+
+public class Btn2_ extends JButton{
+
+
+
+
+    private boolean over;
+    private Color bg;
+    private Color fg;
+    private Color hover_bg;
+    private Color active_bg;
+    private Color hover_fg;
+    private Color active_fg;
+    
+
+
+}
