b6bf8bc3da9a8233558a05804fb82ebdb3d00ec6
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-20 07:56:38 +0530

    Update Combobox.java

--- src/Swing/Combobox.java
+++ src/Swing/Combobox.java
@@ -219,7 +219,25 @@ public class Combobox<E> extends JComboBox<E> {
             g2.dispose();
         }
 
-
+        private void createHintText(Graphics2D g2) {
+            Insets in = getInsets();
+            g2.setColor(skin.wc3);
+            FontMetrics ft = g2.getFontMetrics();
+            Rectangle2D r2 = ft.getStringBounds(combo.getLabeText(), g2);
+            double height = getHeight() - in.top - in.bottom;
+            double textY = (height - r2.getHeight()) / 2;
+            double size;
+            if (animateHinText) {
+                if (show) {
+                    size = 18 * (1 - location);
+                } else {
+                    size = 18 * location;
+                }
+            } else {
+                size = 18;
+            }
+            g2.drawString(combo.getLabeText(), in.right, (int) (in.top + textY + ft.getAscent() - size));
+        }
 
 
 
