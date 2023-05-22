7c321caaef4d1861bc9487caff26ccbb811b0675
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-22 08:01:07 +0530

    Update fmTitle.java

--- src/Swing/fmTitle.java
+++ src/Swing/fmTitle.java
@@ -7,6 +7,12 @@ import javax.swing.JLabel;
 
 public class fmTitle extends JLabel{
     
-
+    public fmTitle(){
+        setOpaque(true);
+        setFont(skin.ttf(20, 2));
+        setForeground(skin.wc4);
+        setBackground(skin.wc8);
+        setPreferredSize(new Dimension(1160, 40));
+    }
 
 }
