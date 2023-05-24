40b42e825670c91364cbcb33aa2a26a47d9da5d2
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-24 08:04:32 +0530

    Update iBtn.java

--- src/Swing/iBtn.java
+++ src/Swing/iBtn.java
@@ -121,7 +121,17 @@ public class iBtn extends JButton{
     private Color active_fg;
     
 
-
+    @Override
+    protected void paintComponent(Graphics grphcs){
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        
+        g2.setColor(getBackground());
+        
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 0, 0);
+                
+        super.paintComponent(grphcs);
+    }
     
 
 }
