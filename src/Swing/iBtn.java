f67745425cfe6ece22376014bf37786502a64f67
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-24 08:04:20 +0530

    Update iBtn.java

--- src/Swing/iBtn.java
+++ src/Swing/iBtn.java
@@ -14,6 +14,28 @@ import java.awt.Dimension;
 public class iBtn extends JButton{
 
 
+    public boolean isOver() {
+        return over;
+    }
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+    public Color getBg() {
+        return bg;
+    }
+    public void setBg(Color bg) {
+        setBackground(bg);
+    }
+    public Color getFg() {
+        return fg;
+    }
+    public void setFg(Color fg) {
+        setForeground(fg);
+    }
+    public Color getHover_bg() {
+        return hover_bg;
+    }
+
 
     private boolean over;
     private Color bg;
