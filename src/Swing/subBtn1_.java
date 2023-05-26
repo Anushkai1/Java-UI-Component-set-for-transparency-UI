52d7bafcfbcea226e08c938e190bc971e7469426
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-26 08:07:52 +0530

    Update subBtn1_.java

--- src/Swing/subBtn1_.java
+++ src/Swing/subBtn1_.java
@@ -11,3 +11,19 @@ import javax.swing.JButton;
 import Assest.skin;
 import java.awt.Dimension;
 
+public class subBtn1_ extends JButton{
+
+
+
+
+    private boolean over;
+    private Color bg;
+    private Color fg;
+    private Color hover_bg;
+    private Color active_bg;
+    private Color hover_fg;
+    private Color active_fg;
+    private TxFld1[] txtList;
+
+
+}
