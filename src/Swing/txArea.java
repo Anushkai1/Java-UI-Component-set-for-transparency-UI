585ce5670df4122f440fd7e28e770d86fe7781f6
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-27 08:09:29 +0530

    Update txArea.java

--- src/Swing/txArea.java
+++ src/Swing/txArea.java
@@ -8,5 +8,14 @@ import javax.swing.BorderFactory;
 import javax.swing.JTextArea;
 
 public class txArea extends JTextArea{
- 
+    public txArea(){
+     setLineWrap(true);
+     setOpaque(true);
+     setBackground(skin.blc13);
+     setForeground(skin.wc3);
+     setFont(skin.ttf(16, 1));
+     setPreferredSize(new Dimension(520,200));
+     setMargin(new Insets(0,5,0,5));
+     //setBorder(new BorderFactory.);
+    }   
 }
