6e237f00cd1ad42a5a89f6f5f03d0c408c1be7dc
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-28 08:11:59 +0530

    Update TxFCont1_.java

--- src/Swing/TxFCont1_.java
+++ src/Swing/TxFCont1_.java
@@ -15,6 +15,29 @@ public class TxFCont1_ extends javax.swing.JPanel {
     }
     
 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        txFld12 = new Swing.TxFld1();
+
+        setPreferredSize(new java.awt.Dimension(530, 70));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("FIRST NAME");
+        jLabel1.setPreferredSize(new java.awt.Dimension(65, 15));
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(5, 10, 370, -1));
+
+        txFld12.setText("txFld12");
+        txFld12.setMaxLength(9);
+        txFld12.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txFld12KeyReleased(evt);
+            }
+        });
+        add(txFld12, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 25, -1, -1));
+    }// </editor-fold>//GEN-END:initComponents
 
 
 
