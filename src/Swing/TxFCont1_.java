fc4ab32beb0f8a93911d383daef26ffd701d7487
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-28 08:11:52 +0530

    Update TxFCont1_.java

--- src/Swing/TxFCont1_.java
+++ src/Swing/TxFCont1_.java
@@ -7,4 +7,10 @@ import Assest.Regex;
 public class TxFCont1_ extends javax.swing.JPanel {
 
 
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private Swing.TxFld1 txFld12;
+    // End of variables declaration//GEN-END:variables
 }
