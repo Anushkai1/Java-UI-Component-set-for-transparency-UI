f02b974852cba6a915a91dcbf79ece23e120af20
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-30 08:16:02 +0530

    Update TxFContwith.java

--- src/Swing/TxFContwith.java
+++ src/Swing/TxFContwith.java
@@ -3,3 +3,13 @@ package Swing;
 
 import Assest.skin;
 
+public class TxFContwith extends javax.swing.JPanel {
+
+
+
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    // End of variables declaration//GEN-END:variables
+}
