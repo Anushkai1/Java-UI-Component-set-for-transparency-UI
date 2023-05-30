df83816d4223bfc9d3af9918d39266638cbfb7d3
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-30 08:16:08 +0530

    Update TxFContwith.java

--- src/Swing/TxFContwith.java
+++ src/Swing/TxFContwith.java
@@ -14,7 +14,19 @@ public class TxFContwith extends javax.swing.JPanel {
     }
     
 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
+        setPreferredSize(new java.awt.Dimension(530, 70));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setText("FIRST NAME");
+        jLabel1.setPreferredSize(new java.awt.Dimension(65, 15));
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(5, 10, 370, -1));
+    }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
