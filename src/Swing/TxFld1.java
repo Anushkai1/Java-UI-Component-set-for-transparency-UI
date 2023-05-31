1a32be86d190fdb7b875e80084d5d6ca579a1f8d
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-31 08:17:24 +0530

    Update TxFld1.java

--- src/Swing/TxFld1.java
+++ src/Swing/TxFld1.java
@@ -24,6 +24,84 @@ import  Assest.Regex;
 
 public class TxFld1 extends JTextField{
 
+    /**
+     * @return the oldVal
+     */
+    public String getOldVal() {
+        return oldVal;
+    }
+
+    /**
+     * @param oldVal the oldVal to set
+     */
+    public void setOldVal(String oldVal) {
+        this.oldVal = oldVal;
+    }
+
+    public Boolean getRequired() {
+        return Required;
+    }
+    public void setRequired(Boolean Required) {
+        this.Required = Required;
+    }
+
+    public int getMaxLength() {
+        return maxLength;
+    }
+
+    public void setMaxLength(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    public Color getLn() {
+        return ln;
+    }
+    public void setLn(Color ln) {
+        this.ln = ln;
+    }
+    public Color getBg() {
+        return bg;
+    }
+    public void setBg(Color bg) {
+        this.bg = bg;
+    }
+    public Color getHoverC() {
+        return hoverC;
+    }
+    public void setHoverC(Color hoverC) {
+        this.hoverC = hoverC;
+    }
+    public Color getActiveC() {
+        return activeC;
+    }
+    public void setActiveC(Color activeC) {
+        this.activeC = activeC;
+    }
+    public Color getErrorC() {
+        return errorC;
+    }
+    public void setErrorC(Color errorC) {
+        this.errorC = errorC;
+    }
+    public Boolean getVldInput() {
+        return vldInput;
+    }
+    public void setVldInput(Boolean vldInput) {
+        this.vldInput = vldInput;
+    }
+    public subBtn getSb1() {
+        return sb1;
+    }
+    public void setSb1(subBtn sb1) {
+        this.sb1 = sb1;
+    }
+    public subBtn getSb2() {
+        return sb2;
+    }
+    public void setSb2(subBtn sb2) {
+        this.sb2 = sb2;
+    }
+    
 
 
     private Boolean vldInput;
