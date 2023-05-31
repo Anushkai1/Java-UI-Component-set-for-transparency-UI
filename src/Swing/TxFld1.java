d07fbff02bc57df16bbcf55cc4fd2300d0266746
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-05-31 08:17:18 +0530

    Update TxFld1.java

---
+++ src/Swing/TxFld1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Swing;
+import Assest.skin;
+import javax.swing.JTextField;
+import Swing.Btn1_;
+import Swing.Btn2_;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import  Assest.Regex;
+
