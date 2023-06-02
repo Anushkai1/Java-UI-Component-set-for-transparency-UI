5e728e97c9e81ffbe6f03f827896ab8c45124e15
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-02 08:23:00 +0530

    Update Main.java

---
+++ src/hspmng/Main.java
@@ -0,0 +1,12 @@
+
+package hspmng;
+
+public class Main {
+       public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new regexcech().setVisible(true);
+            }
+        });
+    } 
+}
