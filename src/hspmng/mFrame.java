8f5d38a60eb17ec34a57ee6e8f37c138673571e5
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-04 08:31:28 +0530

    Update mFrame.java

--- src/hspmng/mFrame.java
+++ src/hspmng/mFrame.java
@@ -14,4 +14,42 @@ public class mFrame extends javax.swing.JFrame {
 
 
 
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private Swing.hBtn Hbpers;
+    private Swing.iBtn MinimizeBtn;
+    private Swing.iBtn SettingBtn;
+    private javax.swing.JLabel bg_label;
+    private Swing.Btn1_ btn1_1;
+    private Swing.Btn2_ btn2_2;
+    private Swing.Btn3_ btn3_1;
+    private Swing.Btn4_ btn4_1;
+    private Swing.Btn4_ btn4_2;
+    private Swing.Btn4_ btn4_3;
+    private Swing.Combobox combobox1;
+    private Swing.Combobox combobox2;
+    private Swing.fmBg fmBg1;
+    private Swing.fmTitle fmTitle1;
+    private Swing.hBtn hBAdmit;
+    private Swing.hBtn hBDash;
+    private Swing.hBtn hBDeath;
+    private Swing.hBtn hBDisc;
+    private Swing.hBtn hBDoc;
+    private Swing.hBtn hBSur;
+    private Swing.hBtn hBWard;
+    private Swing.hBtn hBbirth;
+    private javax.swing.JScrollPane jScrollPane3;
+    private Swing.txArea txArea2;
+    private Swing.TxFCont1_ txFCont1_1;
+    private Swing.TxFCont1_ txFCont1_2;
+    private Swing.TxFCont1_ txFCont1_3;
+    private Swing.TxFCont1_ txFCont1_4;
+    private Swing.TxFCont1_ txFCont1_5;
+    private Swing.TxFCont1_ txFCont1_6;
+    private Swing.TxFContwith txFContwith1;
+    private Swing.TxFContwith txFContwith2;
+    private Swing.TxFld1 txFld11;
+    private Elements.WCloseBtn wCloseBtn;
+    private Elements.WMover wMover;
+    // End of variables declaration//GEN-END:variables
 }
