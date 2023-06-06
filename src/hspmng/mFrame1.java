2742b9f0ff7c479e736ce1f068fa9addc4a6ef26
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-06 08:37:38 +0530

    Update mFrame1.java

---
+++ src/hspmng/mFrame1.java
@@ -0,0 +1,13 @@
+
+package hspmng;
+
+import Swing.hBtn;
+import java.awt.Color;
+import javax.swing.JScrollBar;
+import Elements.ScrollBarCustome;
+import Assest.skin;
+
+public class mFrame1 extends javax.swing.JFrame {
+
+
+}
