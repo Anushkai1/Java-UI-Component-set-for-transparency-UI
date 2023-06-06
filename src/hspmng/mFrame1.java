29114853b93f132850e1c0901fcca464ed5b2b8a
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-06-06 08:37:40 +0530

    Update mFrame1.java

--- src/hspmng/mFrame1.java
+++ src/hspmng/mFrame1.java
@@ -10,4 +10,25 @@ import Assest.skin;
 public class mFrame1 extends javax.swing.JFrame {
 
 
+
+
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private Swing.hBtn Hbpers;
+    private Swing.iBtn MinimizeBtn;
+    private Swing.iBtn SettingBtn;
+    private javax.swing.JLabel bg_label;
+    private Swing.hBtn hBAdmit;
+    private Swing.hBtn hBDash;
+    private Swing.hBtn hBDeath;
+    private Swing.hBtn hBDisc;
+    private Swing.hBtn hBDoc;
+    private Swing.hBtn hBSur;
+    private Swing.hBtn hBWard;
+    private Swing.hBtn hBbirth;
+    private javax.swing.JScrollPane jScrollPane1;
+    private hspmng.NewJPanel newJPanel2;
+    private Elements.WCloseBtn wCloseBtn;
+    private Elements.WMover wMover;
+    // End of variables declaration//GEN-END:variables
 }
