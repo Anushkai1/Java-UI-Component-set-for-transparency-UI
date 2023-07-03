b38256d648c5bb816dba170234aa9e86c97846fc
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-07-03 08:47:33 +0530

    Update regexcech.java

--- src/hspmng/regexcech.java
+++ src/hspmng/regexcech.java
@@ -13,5 +13,24 @@ public class regexcech extends javax.swing.JFrame {
 
 
     
-
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private Swing.hBtn Hbpers;
+    private Swing.iBtn MinimizeBtn;
+    private Swing.iBtn SettingBtn;
+    private javax.swing.JLabel bg_label;
+    private Swing.Combobox combobox1;
+    private Swing.hBtn hBAdmit;
+    private Swing.hBtn hBDash;
+    private Swing.hBtn hBDeath;
+    private Swing.hBtn hBDisc;
+    private Swing.hBtn hBDoc;
+    private Swing.hBtn hBSur;
+    private Swing.hBtn hBWard;
+    private Swing.hBtn hBbirth;
+    private Swing.subBtn subBtn2;
+    private Swing.TxFld1 txFld11;
+    private Swing.TxFld1 txFld13;
+    private Elements.WCloseBtn wCloseBtn;
+    private Elements.WMover wMover;
+    // End of variables declaration//GEN-END:variables
 }
